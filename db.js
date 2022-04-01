fd48e4eb5349711b5c5118dadb8d43f7abfb5959
clarophil <dkp@ecam.be>
2022-04-01 18:02:36 +0200

    fix

--- db.js
+++ db.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
+
 const Sequelize = require('sequelize')
 const sequelize = new Sequelize(
-    'users', 
-    'root', 
-    'root',  {
+    'users',
+    process.env.DB_USER,
+    process.env.DB_PASSWORD, {
     dialect: 'mysql',
     host: 'localhost'
-   }
+}
 );
 module.exports = sequelize
--- server.js
+++ server.js
@@ -10,18 +10,26 @@ app.use(cors());
 
 app.use(express.json());
 
-const Sequelize = require('sequelize')
-const db = require('./db.js')
+require('dotenv').config();
+
+// Importing the database model
+// const Sequelize = require('sequelize')
+// const db = require('./db.js')
 
 // Creating all the tables defined in user
-db.sync()
+// db.sync();
 // db.sync({force: true})
 
 let router = require('./routes');
 app.use("/", router)
 
- // Launch app to listen to specified port
- const port = 8088;
+// Manage bad route
+app.use(function (req, res, next) {
+    res.status(404).json({ "error": "path not found" });
+});
+
+// Launch app to listen to specified port
+const port = process.env.PORT || 8080;
 app.listen(port, function () {
-    console.log('Runnings on port ') + port ;
-})
+    console.log('Runnings on ' + process.env.SERVER + port);
+});
